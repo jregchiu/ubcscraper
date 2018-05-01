d4b2f93b95bd5f7d750b92673977d4667a231409
Jeremy Chiu <8358221+jregchiu@users.noreply.github.com>
2018-05-01 15:18:37 -0700

    fix scraper to parse multi-term sections

--- ubcscraper/spiders/ubc_spider.py
+++ ubcscraper/spiders/ubc_spider.py
@@ -4,6 +4,7 @@ from ubcscraper.items import Section
 
 class UBCSpider(scrapy.Spider):
     name = 'UBC'
+    filters = ('Unreleased', 'Cancelled', 'STT', 'Waiting List')
 
     def start_requests(self):
         yield scrapy.Request('https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=0&sessyr={0}&sesscd={1}'.format(self.year, self.session.upper()), self.parse)
@@ -15,11 +16,14 @@ class UBCSpider(scrapy.Spider):
                 yield response.follow(href, self.parse_department)
 
     def parse_department(self, response):
-        for href in response.xpath('body/div/div/table//a/@href'):
+        for href in response.xpath('body/div/div/table//@href'):
             yield response.follow(href, self.parse_course)
 
     def parse_course(self, response):
-        for href in response.xpath('body/div/div/table//a/@href'):
+        for row in response.xpath('body/div/div/table/tr'):
+            if any(f in self.filters for f in [str.strip(s) for s in row.xpath('./td[1]/text()|./td[3]/text()').extract()]) or '' in [str.strip(s) for s in row.xpath('./td[6]/text()|./td[7]/text()|./td[8]/text()').extract()] or not row.xpath('./td[2]/a/@href'):
+                continue
+            href = row.xpath('./td[2]/a/@href')[0]
             yield response.follow(href, self.parse_section)
 
     def parse_section(self, response):
@@ -30,9 +34,9 @@ class UBCSpider(scrapy.Spider):
             sl.add_value('year', self.year)
             sl.add_value('session', self.session.upper())
             sl.add_xpath('code', 'body/div/div/h4', re='(\w{4}\s\w{3,4}\s\w{3,4})')
-            sl.add_xpath('activity', 'body/div/div/h4', re='\((\w+)\)')
-            sl.add_value('term', td[i].re(r'(\d+)'))
-            sl.add_value('days', td[i+1].re(r'(\w{3})'))
-            sl.add_value('start', td[i+2].extract())
-            sl.add_value('end', td[i+3].extract())
+            sl.add_xpath('activity', 'body/div/div/h4', re='\((.+)\)')
+            sl.add_value('term', td[0 + 6 * i].re(r'(\d+)'))
+            sl.add_value('days', td[1 + 6 * i].re(r'(\w{3})'))
+            sl.add_value('start', td[2 + 6 * i].extract())
+            sl.add_value('end', td[3 + 6 * i].extract())
             yield sl.load_item()
